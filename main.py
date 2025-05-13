6c1e54027ecd8a8e824c90d854ae1f865ecb090d
seonjiwon <tommys915@gmail.com>
2025-05-13 16:23:53 +0900

    feat: /analysis endpoint setting

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Depends
 
-from routes import test_router, transcribe_router
+from routes import analysis_router, transcribe_router
 
 from core.lifespan import lifespan
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(test_router.router)
+app.include_router(analysis_router.router)
 
 app.include_router(transcribe_router.router)
